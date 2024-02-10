c3b9e85298cd3a56750620f341a88e958a4f8754
home <nuttapong_coach@hotmail.com>
2024-02-10 17:17:38 +0700

    revise1st

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,25 +1,33 @@
 "use client"
 
-import { create } from "@/actions/create-board"
 import { Button } from "@/components/ui/button"
 import { useFormState } from "react-dom"
+import { FormInput } from "./form-input"
+import { FormButton } from "./form-button"
+import { useAction } from "@/hooks/use-action"
+import { createBoard } from "@/actions/create-board"
 
 export const Form = () => {
-    const initialState = { message: null, errors: {} }
-    const [state, dispatch] = useFormState(create, initialState)
+    const { execute, fieldErrors } = useAction(createBoard, {
+      onSuccess: (data) => {
+        console.log(data, "SUCCESS!")
+      },
+      onError: (error) => {
+        console.error(error)
+      }
+    })
 
+    const onSubmit = (formData: FormData) => {
+      const title = formData.get("title") as string
+
+      execute({ title })
+    }
     return (
-        <form action={dispatch}>
-          <input 
-            id="title"
-            name="title"
-            required
-            placeholder="Enter a board title"
-            className="border-black border p-1"
-          />
-          <Button type="submit">
-            Submit  
-          </Button>
+        <form action={onSubmit}>
+          <div className="flex flex-col space-y-2">
+            <FormInput errors={fieldErrors} />
+          </div>
+          <FormButton />
         </form>
     )
 }
