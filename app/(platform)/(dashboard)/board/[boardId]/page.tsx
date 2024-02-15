2fcfeee868db9367fce37c613ff53b98713d314c
home <nuttapong_coach@hotmail.com>
2024-02-15 22:38:01 +0700

    add list

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,50 @@
-const BoardIdPage = () => {
+import { auth } from "@clerk/nextjs"
+
+import { db } from "@/lib/db"
+
+import { redirect } from "next/navigation"
+import { Listcontainer } from "./_components/list-container"
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string
+  }
+}
+
+const BoardIdPage = async ({
+  params
+}: BoardIdPageProps) => {
+  const { orgId } = auth()
+  
+  if (!orgId) {
+    redirect("select-org")
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId
+      }
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc"
+        }
+      }
+    },
+    orderBy: {
+      order: "asc"
+    }
+  })
+
     return (
-        <div>
-          Board ID!
+        <div className="p-4 h-full overflow-x-auto">
+          <Listcontainer 
+            boardId={params.boardId}
+            data={lists}
+          />
         </div>
     )
 }
